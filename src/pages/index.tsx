c300980746fdce31f3e3303738ae97337275eafe
kotaro1574 <shake1574@gmail.com>
2021-09-12 12:43:37 +0900

    Header作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,14 @@ import {
   AboutChocomintou,
   RecommendItems,
   ChocomintKatsu,
+  Header
 } from '../components/pages/home/index';
 import { BaseDivider } from '../components/uiParts/index';
 
 const Home: React.FC = () => {
   return (
     <div>
+      <Header />
       <FirstView />
       <RecommendItems />
       <BaseDivider />
