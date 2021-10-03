9605645b55927114016bda3d545297e7fb6ccc5b
Syunki0403 <blackapple0403@gmail.com>
2021-10-03 14:23:29 +0900

    軽微修正

--- src/styles/js/theme.ts
+++ src/styles/js/theme.ts
@@ -1,7 +1,6 @@
 import { createTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 import { COLORS } from '../../const/color';
-import { NoEncryption } from '@material-ui/icons';
 
 const theme = createTheme({
   overrides: {
