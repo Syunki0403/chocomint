18281463c38a63261b0ee29ab44b943825a41073
kotaro1574 <shake1574@gmail.com>
2021-10-17 13:18:38 +0900

    imageサイズ変更

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,7 +5,7 @@ import logo from '/public/images/logo_transparent.png';
 const Footer = () => {
   return (
     <div className="p-2 text-center" style={{ background: '#68e8d8' }}>
-      <Image src={logo} alt="ロゴ" width={150} height={150} className="img-noBlurred mx-2" />
+      <Image src={logo} alt="ロゴ" width={120} height={120} className="img-noBlurred mx-2" />
       <div className="flex justify-center mb-6">
         <p className="cursor-pointer">検索 |</p>
         <p className="cursor-pointer">お問い合わせ |</p>
